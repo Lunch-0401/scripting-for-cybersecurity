5faad47e7b0afe01a7bb2ce325de34088f6155f2
Lunch-0401 <c00321982@setu.ie>
2026-09-14 14:43:11 +0000

    Complete Lab 1

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+stuNum = input("What is your student number?")
+year = input("What is your year of study?")
+
+print("Name: " + name)
+print("Student Number: " + stuNum)
+print("Year: " + year)
